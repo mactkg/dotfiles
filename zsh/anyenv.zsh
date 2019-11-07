2769e1d0421d0a61555c02e4eafd4ff7e3388c6e
mactkg <mactkg@gmail.com>
2019-11-08 08:15:21 +0900

    Use anyenv in my personal machine

--- zsh/anyenv.zsh
+++ zsh/anyenv.zsh
@@ -1,14 +1,3 @@
-if [ `hostname -s` = "mactkg" ]; then
-  export RBENV_ROOT=/usr/local/var/rbenv
-  export PYENV_ROOT=/usr/local/opt/pyenv
-  eval "$(rbenv init - zsh)"
-  eval "$(plenv init -)"
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-else
-  export PATH="$HOME/.anyenv/bin:$PATH"
-  export PATH=$HOME/.anyenv/envs/ndenv/shims:$PATH
-  eval "$(anyenv init - zsh)"
-fi
-
-export PATH=$RBENV_ROOT/shims:$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH
+export PATH="$HOME/.anyenv/bin:$PATH"
+export PATH=$HOME/.anyenv/envs/nodenv/shims:$PATH
+eval "$(anyenv init - zsh)"
